71e499f5290a5c83403705a1c02081348ef56191
Yohans Bekele <yohansbekele@Yohanss-MacBook-Air.local>
2024-12-31 01:19:23 +0100

    ui fix

--- src/Components/LampDemo.jsx
+++ src/Components/LampDemo.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { LampContainer } from "../Components/ui/lamp"; //
 
-import { Button } from "../components/ui/moving-border";
+import { Button } from "../Components/ui/moving-border";
 /**
  * LampDemo Component
  * Displays a demo with animated heading inside the LampContainer.
